d739c52e378c1f9481d86b483ca4061307d485c4
moideeenmashad <moideenmashad@gmail.com>
2022-11-12 23:07:39 +0530

    validation 2nd changes

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 
 
 let pwd = document.getElementById("pwd");
+let email = document.getElementById("email");
+let error = document.getElementById("error");
+
 
 
 // password validation
@@ -9,15 +12,40 @@ let pwd = document.getElementById("pwd");
 function passvalidate(){
     if (pwd.value.trim() == ""){
         alert("incorect password");
+        pwd.style.color = "red";
         return false;
     }
     else if (pwd.value <= 8){
         alert("password is too short");
-        pwd.style.color = "red"
+        pwd.style.color = "red";
         return false;
     }
     else{
         return true;
     }
+
+
+
+    // let regexp2 = /^([+\0-9{10,12}]+)$/
+    //   if (regexp2.test(pwd)){
+
+    //   }
     
+}
+
+// email validation
+
+function emailvalidate(){
+    let regexp = /^([A-Za-z0-9\.-]+)@([a-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/
+    if(error.test(email)){
+        error.innerHTML = "valid email";
+        error.style.color ="red";
+        return true;
+    }
+    else{
+        error.innerHTML = "invalid email";
+        error.style.color = "green";
+        return false;
+    }
+
 }
